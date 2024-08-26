d507b7eda7156ff5730ecd249e6c772c84fe4d4d
smithb024 <bertram.smith@gmail.com>
2024-08-26 21:18:18 +0100

    #11: Style

--- NynaeveLib/Commands/CommonCommandLambda.cs
+++ NynaeveLib/Commands/CommonCommandLambda.cs
@@ -1,63 +1,67 @@
 ﻿namespace NynaeveLib.Commands
 {
-  using System;
-  using System.Windows.Input;
-
-  public class CommonCommand<T> : ICommand
-  {
-    public CommonCommand(Action<T> command)
-      : this(command, null)
-    {
-    }
-
-    public CommonCommand(
-      Action<T> command,
-      Predicate<T> canExecuteCommand)
-    {
-      this.RunCommand = command;
-      this.CanRunCommand = canExecuteCommand;
-    }
+    using System;
+    using System.Windows.Input;
 
     /// <summary>
-    /// Run command action.
+    /// The generic common command class.
     /// </summary>
-    public Action<T> RunCommand
+    /// <typeparam name="T"></typeparam>
+    public class CommonCommand<T> : ICommand
     {
-      get;
-      private set;
-    }
+        /// <summary>
+        /// Initialises a new <see cref="CommonCommand{T}"/> class.
+        /// </summary>
+        /// <param name="command">The command to run.</param>
+        public CommonCommand(Action<T> command)
+          : this(command, null)
+        {
+        }
 
-    /// <summary>
-    /// Can run command function.
-    /// </summary>
-    public Predicate<T> CanRunCommand
-    {
-      get;
-      private set;
-    }
+        public CommonCommand(
+          Action<T> command,
+          Predicate<T> canExecuteCommand)
+        {
+            this.RunCommand = command;
+            this.CanRunCommand = canExecuteCommand;
+        }
 
-    /// <summary>
-    /// Determine if the command can be run.
-    /// </summary>
-    /// <param name="parameter">not used</param>
-    /// <returns>always true</returns>
-    public bool CanExecute(object parameter)
-    {
-      return this.CanRunCommand == null ? true : this.CanRunCommand((T)parameter);
-    }
+        /// <summary>
+        /// Run command action.
+        /// </summary>
+        public Action<T> RunCommand { get; private set; }
 
-    /// <summary>
-    /// Can execute changed event.
-    /// </summary>
-    public event EventHandler CanExecuteChanged
-    {
-      add { CommandManager.RequerySuggested += value; }
-      remove { CommandManager.RequerySuggested -= value; }
-    }
+        /// <summary>
+        /// Can run command function.
+        /// </summary>
+        public Predicate<T> CanRunCommand { get; private set; }
 
-    public void Execute(object parameter)
-    {
-      this.RunCommand((T)parameter);
+        /// <summary>
+        /// Determine if the command can be run.
+        /// </summary>
+        /// <param name="parameter">not used</param>
+        /// <returns>always true</returns>
+        public bool CanExecute(object parameter)
+        {
+            return this.CanRunCommand == null ? true : this.CanRunCommand((T)parameter);
+        }
+
+        /// <summary>
+        /// Can execute changed event.
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        /// <summary>
+        /// Execute <paramref name="parameter"/>.
+        /// </summary>
+        /// <param name="parameter">the object to execute.</param>
+        public void Execute(object parameter)
+        {
+            this.RunCommand((T)parameter);
+        }
     }
-  }
 }
