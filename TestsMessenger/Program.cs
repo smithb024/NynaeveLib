ee981f81a7020197ea73bfa908a2204504841515
smithb024 <bertram.smith@gmail.com>
2024-08-31 12:54:10 +0100

    #11: Create a test app for the messenger

---
+++ TestsMessenger/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TestsMessenger
+{
+    using System;
+
+    /// <summary>
+    /// Main program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main method.
+        /// </summary>
+        /// <param name="args">Input arguments</param>
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
