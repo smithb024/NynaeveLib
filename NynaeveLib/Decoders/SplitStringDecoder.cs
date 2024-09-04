f996ab2bd66d945b19198e2d66d6baca796b8903
smithb024 <bertram.smith@gmail.com>
2024-09-04 22:21:57 +0100

    #11: Tidy

--- NynaeveLib/Decoders/SplitStringDecoder.cs
+++ NynaeveLib/Decoders/SplitStringDecoder.cs
@@ -1,50 +1,48 @@
 ﻿namespace NynaeveLib.Decoders
 {
-  using System;
-  using System.Collections.Generic;
-  using System.Linq;
-  using System.Text;
-  using System.Threading.Tasks;
+    using System;
+    using System.Linq;
 
-  /// <summary>
-  /// Static class used to determine sections of strings
-  /// </summary>
-  public static class SplitStringDecoder
-  {
     /// <summary>
-    /// Split the incoming string using the delimiter. Return the contents of the requested cell
-    /// if present.
+    /// Static class used to determine sections of strings
     /// </summary>
-    /// <param name="inputString">string to split</param>
-    /// <param name="delimiter">delimiter character</param>
-    /// <param name="cell">section number to return</param>
-    /// <returns>section indicated by the cell</returns>
-    public static string ReturnSectionOfString(string inputString,
-                                               char delimiter,
-                                               int cell)
+    public static class SplitStringDecoder
     {
-      // Consistency check
-      if (
-        cell <= 0 ||
-        string.IsNullOrEmpty(inputString))
-      {
-        return string.Empty;
-      }
+        /// <summary>
+        /// Split the incoming string using the delimiter. Return the contents of the requested cell
+        /// if present.
+        /// </summary>
+        /// <param name="inputString">string to split</param>
+        /// <param name="delimiter">delimiter character</param>
+        /// <param name="cell">section number to return</param>
+        /// <returns>section indicated by the cell</returns>
+        public static string ReturnSectionOfString(
+            string inputString,
+            char delimiter,
+            int cell)
+        {
+            // Consistency check
+            if (
+              cell <= 0 ||
+              string.IsNullOrEmpty(inputString))
+            {
+                return string.Empty;
+            }
 
-      string[] cells = null;
+            string[] cells = null;
 
-      // Ensure that the input contains the delimiter and that the cell exists.
-      if (inputString.Contains(delimiter))
-      {
-        cells = inputString.Split(delimiter);
+            // Ensure that the input contains the delimiter and that the cell exists.
+            if (inputString.Contains(delimiter))
+            {
+                cells = inputString.Split(delimiter);
 
-        return cells.Count() >= cell ?
-          cells[cell - 1] :
-          string.Empty;
-      }
+                return cells.Count() >= cell ?
+                  cells[cell - 1] :
+                  string.Empty;
+            }
 
-      // No delimiter present, only return full string if first cell is requested.
-      return cell == 1 ? inputString : string.Empty;
+            // No delimiter present, only return full string if first cell is requested.
+            return cell == 1 ? inputString : string.Empty;
+        }
     }
-  }
 }
